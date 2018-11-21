fe459e9f2149fb96c5b324dd2401e69de2d61220
PeterHu <peterhu@peterhude-macbook-pro.local>
2018-11-21 16:25:37 +0800

    BMI complete

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightTextfield: UITextField!
+    @IBOutlet weak var weightTextfield: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func start(_ sender: Any) {
+        if let height = heightTextfield.text,let weight = weightTextfield.text,let heightValue = Double(height),let weightValue = Double(weight)
+        {
+            let heightInMeter = heightValue / 100
+            let BMI = weightValue / (heightInMeter * heightInMeter)
+            let format = String(format: "%.1f", BMI)
+            result.text = format
+            if(BMI > 24.0){
+                statusLabel.text = "TOO FAT"
+            }
+            else if(BMI < 18.5){
+                statusLabel.text = "TOO THIN"
+            }
+            else{
+                statusLabel.text = "GOOD SHAPE"
+            }
+        }
+    }
+    
 
 }
 
